f0aa1fc9064afea025c2ef159d5b1997f88f1985
shreya0777628 <shreyagarg0777@gmail.com>
2025-09-17 02:36:53 -0400

    Create ques 2.cpp

---
+++ assignment 3/ques 2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main(){
+    string s; cin>>s;
+    stack<char> st;
+    for(char c:s) st.push(c);
+    while(!st.empty()){ cout<<st.top(); st.pop(); }
+}
